131c1696f4714239ce2a980bdb3c24a6a83d3fd3
navazjm <michaelnavs@gmail.com>
2025-01-29 22:11:17 -0600

    api: moved healthcheck handler to routes.go

--- internal/webapp/routes.go
+++ internal/webapp/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 
+	"github.com/navazjm/pixelarcade/internal/webapp/utils/json"
 	"github.com/navazjm/pixelarcade/internal/webapp/utils/response"
 )
 
@@ -27,4 +28,19 @@ func (app *Application) Routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/api/healthcheck", app.healthcheckHandler)
 
 	return app.recoverPanic(app.secureHeaders(app.logRequest(app.enableCORS(app.validateOrigin(app.rateLimit(router))))))
+}
+
+func (app *Application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	env := json.Envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.Config.Env,
+			"version":     app.Config.Version,
+		},
+	}
+
+	err := json.Write(w, http.StatusOK, env, nil)
+	if err != nil {
+		response.ServerError(w, r, app.Logger, err)
+	}
 }

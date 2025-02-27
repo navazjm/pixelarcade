c1f76573b6938414b6343b44652a2eae8ead262f
navazjm <michaelnavs@gmail.com>
2025-02-27 17:38:47 -0600

    [BE] added database.ErrMockDatabase

--- internal/webapp/utils/database/error.go
+++ internal/webapp/utils/database/error.go
@@ -6,4 +6,5 @@ var (
 	ErrRecordNotFound = errors.New("record not found")
 	ErrEditConflict   = errors.New("edit conflict")
 	ErrDuplicateEmail = errors.New("duplicate email")
+	ErrMockDatabase   = errors.New("mock database error")
 )
